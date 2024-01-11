45db65bebb90cbf69a833844ed70e6d7224420b2
akitou <kitouanna@gmail.com>
2024-01-11 12:55:55 +0200

    Adding API Project

--- HR.LeaveManagement.Application/Features/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
+++ HR.LeaveManagement.Application/Features/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -8,6 +8,7 @@ namespace HR.LeaveManagement.Application.Features.Queries.GetAllLeaveTypes
 {
     public class LeaveTypeDto
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int DefaultDays { get; set; }
     }
