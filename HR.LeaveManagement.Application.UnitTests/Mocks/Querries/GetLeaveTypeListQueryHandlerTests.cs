50c38e3dc99ad1fe0cffac6bd11286a9d9cf6fa9
akitou <kitouanna@gmail.com>
2024-01-24 17:20:36 +0200

    .

---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/Querries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.MappingProfiles;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks.Querries
+{
+    public class GetLeaveTypeListQueryHandlerTests
+    {
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private IMapper _mapper;
+        private Mock<IAppLogger<GetLeaveTypesQueryHandler>> _mockAppLogger;
+
+        public GetLeaveTypeListQueryHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<LeaveTypeProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+            _mockAppLogger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypesQueryHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
+        }
+    }
+}
